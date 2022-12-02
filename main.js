6e11c70b9296fb84ab99f5f1a8a103f00e66e12a
Vahid <vahidsuleimanov@MacBook-Pro-vahid.local>
2022-12-02 15:22:38 +0600

    first commit

--- main.js
+++ main.js
@@ -28,8 +28,30 @@ let accordionBody = document.querySelector("#accordion__body");
 //   accordion
 // );
 
+// paginate
+let prevBtn = document.querySelector("#prev-btn");
+let nextBtn = document.querySelector("#next-btn");
+let currentPage = 1;
+let limit = 4;
+
+// filter
+let form = document.querySelector("form");
+let category = "all";
+
 function adminReturn() {
-  if (code == "12") {
+  if (code != "12") {
+    setTimeout(() => {
+      for (let i of admin_panel_arr) {
+        i.style.display = "none";
+      }
+    }, 50);
+    section_add.style.display = "none";
+  } else {
+    setTimeout(() => {
+      for (let i of admin_panel_arr) {
+        i.style.display = "block";
+      }
+    }, 50);
     section_add.style.display = "block";
   }
 }
@@ -41,7 +63,24 @@ clickAdmin.addEventListener("click", () => {
 
 // тег для отображения  данных в браузере
 let sectionRead = document.getElementById("section__read");
+let inpEditDetails = document.querySelector(".window__edit_details");
+let inpEditPrice = document.querySelector(".window__edit_price");
+let inpEditQuantity = document.querySelector(".window__edit_quantity");
+let inpEditSales = document.querySelector(".window__edit_sales");
+let inpEditCategory = document.querySelector(".window__edit_category");
+let inpEditUrl = document.querySelector(".window__edit_url");
+let btnEditAdd = document.querySelector(".window__edit_btn-save");
+
 // console.log("section__read");
+let btnCloseModal = document.querySelector(".window__edit_close");
+let mainModal = document.querySelector(".main-modal");
+// console.log(mainModal);
+// console.log(btnCloseModal);
+
+//
+let inpSearch = document.querySelector(".search-txt");
+
+let searchValue = inpSearch.value;
 
 //! ===== ACCORDION START =======
 
@@ -67,6 +106,7 @@ async function createProduct(obj) {
     },
     body: JSON.stringify(obj),
   }).then((res) => res.json);
+  readProducts();
 }
 
 btnAdd.addEventListener("click", () => {
@@ -101,9 +141,14 @@ btnAdd.addEventListener("click", () => {
 // ! ===============   READ START     ============================
 
 async function readProducts() {
-  let data = await fetch(API).then((res) => res.json());
+  let data = await fetch(
+    `${API}?q=${searchValue}&_page=${currentPage}&_limit=${limit}&${
+      category === "all" ? "" : "category=" + category
+    }`
+  ).then((res) => res.json());
   // console.log(data);
   sectionRead.innerHTML = "";
+  console.log(data);
   data.forEach((item) => {
     // let productCard = document.createElement("div")
     sectionRead.innerHTML += `
@@ -130,11 +175,117 @@ async function readProducts() {
     <button class=""btnBuy>Выбрать</button>
     </div>
     <div class="admin-panel" id="admin">
-    <img src ="https://cdn-icons-png.flaticon.com/512/1799/1799391.png" width="20px" />
-    <img src ="https://www.freeiconspng.com/thumbs/edit-icon-png/edit-new-icon-22.png" width="20px"/>
+    <img id=${item.id} onclick="deleteProduct(${item.id})" src ="https://cdn-icons-png.flaticon.com/512/1799/1799391.png" width="20px" />
+    <img src ="https://www.freeiconspng.com/thumbs/edit-icon-png/edit-new-icon-22.png" width="20px" onclick="handEditBtn(${item.id})"/>
     </div>
     </div>
     </div>`;
   });
+  pageTotal();
+  adminReturn();
 }
 readProducts();
+// ! READ END
+
+// ! ========== DELETE START =============
+
+async function deleteProduct(id) {
+  await fetch(`${API}/${id}`, {
+    method: "DELETE",
+  }).then((res) => res.json());
+  readProducts();
+}
+
+// ! DELETE END
+
+// ! EDIT START
+async function editProduct(id, editeObj) {
+  if (
+    !inpEditDetails.value.trim() ||
+    !inpEditQuantity.value.trim() ||
+    !inpEditPrice.value.trim() ||
+    !inpEditCategory.value.trim() ||
+    !inpEditSales.value.trim() ||
+    !inpEditUrl.value.trim()
+  ) {
+    alert("Write someone!");
+    return;
+  }
+  await fetch(`${API}/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(editeObj),
+  });
+}
+let editId = "";
+
+async function handEditBtn(id) {
+  mainModal.style.display = "block";
+  let data = await fetch(`${API}/${id}`).then((res) => res.json());
+  inpEditCategory.value = data.category;
+  inpEditDetails.value = data.details;
+  inpEditPrice.value = data.price;
+  inpEditQuantity.value = data.quantity;
+  inpEditSales.value = data.sale;
+  inpEditUrl.value = data.urlImg;
+  // console.log(data);
+  editId = data.id;
+}
+
+btnEditAdd.addEventListener("click", () => {
+  let editeObj = {
+    details: inpDetails.value,
+    price: inpEditPrice.value,
+    quantity: inpEditQuantity.value,
+    category: inpCategory.value,
+    urlImg: inpEditUrl.value,
+    sale: inpEditSales.value,
+  };
+  // console.log(editeObj);
+  editProduct(editId, editeObj);
+  mainModal.style.display = "none";
+});
+
+// ! SEARCH START
+
+inpSearch.addEventListener("input", (e) => {
+  // console.log(e.target.value);
+  searchValue = e.target.value;
+  readProducts();
+});
+
+// ! SEARCH END
+
+// ! PAGINATION START
+
+let countPage = 1;
+async function pageTotal() {
+  let data = await fetch(`${API}?q=${searchValue}`).then((res) => res.json());
+  console.log(data);
+  countPage = Math.ceil(data.lenght / limit);
+}
+
+prevBtn.addEventListener("click", () => {
+  if (currentPage <= 1) return;
+  currentPage--;
+  readProducts();
+});
+nextBtn.addEventListener("click", () => {
+  if (currentPage >= countPage) return;
+  currentPage++;
+  readProducts();
+});
+
+// ! PAGINATION END
+
+// ! ======== FILTER START
+
+form.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  category = e.target.value;
+  readProducts();
+});
+
+// ! FILTER END
